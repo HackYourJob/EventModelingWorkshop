d22877f4b559d4bdc1d5b0c58e88ee21515b142e
Emilien Pecoul (Ouarzy) <emilien@pecoul.com>
2019-09-10 17:16:21 +0200

    working inital project

--- App/Startup.cs
+++ App/Startup.cs
@@ -49,7 +49,7 @@ namespace App
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+//            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Tests
         [Fact]
         public void Test1()
         {
-
+            Assert.True(false);
         }
     }
 }
