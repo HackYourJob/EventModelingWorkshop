c7c2b26f5842d4f8573c82a1333214b2044c9fb2
Aurélien BOUDOUX <aurelien@boudoux.fr>
2019-09-11 14:22:07 +0200

    première vue razor sur la room

---
+++ dotnetTeam/App/Controller/RoomController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controller
+{
+    public class RoomController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        // GET: Room
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+		[Route("[controller]/ToBeChecked")]
+	    public ActionResult ToBeChecked() {
+		    return View();
+	    }
+
+
+		/*
+        // GET: Room/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Room/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Room/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Room/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Room/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Room/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Room/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+		*/
+	}
+}
