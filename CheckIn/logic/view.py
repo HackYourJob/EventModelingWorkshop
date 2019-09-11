409e3a3e71971e5ef8860fb31998fa568939487a
Cédric Foellmi <cedric@onekiloparsec.dev>
2019-09-11 17:20:48 +0200

    Ah la loose!!
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- CheckIn/logic/view.py
+++ CheckIn/logic/view.py
@@ -30,7 +30,7 @@ class CheckinRoomsAvailabilityView(object):
 
         rooms = [{'id': r['id'], 'type': r['type']} for r in self.room_events]
         bookings = [b for b in self.booking_events if str(b['id']) == str(self.booking_id)]
-        if len(bookings) == 1:
+        if len(bookings) == 0:
             raise Exception('There is no valid booking event for id: {}'.format(self.booking_id))
         if len(bookings) > 1:
             raise Exception('There is multiple booking events for id: {}'.format(self.booking_id))
