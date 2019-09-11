91fec101cd86a4de4ec795b7b0cdbd7d3c970c82
Aurélien BOUDOUX <aurelien@boudoux.fr>
2019-09-11 15:31:35 +0200

    integration room checking

--- dotnetTeam/App/Controller/RoomController.cs
+++ dotnetTeam/App/Controller/RoomController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -10,6 +11,13 @@ namespace App.Controller
 {
     public class RoomController : Microsoft.AspNetCore.Mvc.Controller
     {
+	    private readonly IEventsPublisher _publisher;
+
+	    public RoomController(IEventsPublisher publisher)
+	    {
+		    _publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+	    }
+
         // GET: Room
         public ActionResult Index()
         {
@@ -22,90 +30,19 @@ namespace App.Controller
 		    return View();
 	    }
 
-
 	    [Route("[controller]/Checking/{roomId}")]
 		public ActionResult Checking(int roomId)
 	    {
 		    return View(new CheckingModel(roomId));
 	    }
 
-
-		/*
-        // GET: Room/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Room/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Room/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Room/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Room/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Room/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Room/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-		*/
+	    [Route("[controller]/CheckingDone/{roomId}/{status}")]
+	    public ActionResult CheckingDone(int roomId, RoomCheckStatus status) 
+		{
+		    var room = new Room(new RoomId(roomId.ToString()));
+		    room.CheckingDone(_publisher, status);
+			return View(status);
+	    }
 	}
 
 	public class CheckingModel
